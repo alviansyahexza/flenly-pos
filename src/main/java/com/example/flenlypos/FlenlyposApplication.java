9b7c337e6527c349a81116e093511cafdfba5faa
alviansyahexza <alviansyahexza@gmail.com>
2025-01-21 11:03:12 +0700

    init

---
+++ src/main/java/com/example/flenlypos/FlenlyposApplication.java
@@ -0,0 +1,13 @@
+package com.example.flenlypos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlenlyposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlenlyposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flenlypos/FlenlyposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flenlypos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlenlyposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
